23daecb210084df97d6bb8c3936faad137b61127
Roc Tanweer <mohammadtanweer769@gmail.com>
2022-06-10 06:33:09 +0530

    ENABLED: tailwind css for the react and styled hello world

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import "./styles/globals.css";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <h1 className="text-4xl font-weight text-purple-500">Hello World</h1>;
 }
 
 export default App;
