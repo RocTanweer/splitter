714aceb2e31580197c84673e51e9c74f7d773280
Roc Tanweer <mohammadtanweer769@gmail.com>
2022-06-12 10:24:31 +0530

    ADD: RadioBtn comp along with its state handling

---
+++ src/components/RadioBtn.jsx
@@ -0,0 +1,25 @@
+function RadioBtn({ id, radioValue, name, tip, setTip }) {
+  const handleRadioField = (e) => {
+    const currEl = e.target;
+    setTip({ type: "radio", value: +currEl.value });
+  };
+
+  return (
+    <div className="relative w-[117px] h-[48px] text-center">
+      <input
+        type="radio"
+        checked={tip.type === "radio" && tip.value === radioValue}
+        id={id}
+        name={name}
+        value={radioValue}
+        onChange={handleRadioField}
+        className="absolute top-1 left-1 opacity-0 peer"
+      />
+      <label htmlFor={id} className="w-full h-full py-[14px] block bg-cyan-200 text-white text-[20px]  rounded-sm cursor-pointer peer-checked:bg-cyan-100">
+        {`${radioValue}%`}
+      </label>
+    </div>
+  );
+}
+
+export default RadioBtn;
