243d20f697d25d52439929eeb89185f50f803859
Roc Tanweer <mohammadtanweer769@gmail.com>
2022-06-12 10:26:13 +0530

    ADD: global tip state for the tip field on the parent comp App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,21 @@
+import { useState } from "react";
+
 import "../styles/globals.css";
 
 // Custom Components
-import TextInput from "./TextInput";
+// import TextInput from "./TextInput";
+import TipField from "./TipField";
 
 // Media
-import dollarIcon from "../assets/icon-dollar.svg";
-import personIcon from "../assets/icon-person.svg";
+// import dollarIcon from "../assets/icon-dollar.svg";
+// import personIcon from "../assets/icon-person.svg";
 
 function App() {
+  const [tip, setTip] = useState({});
+
   return (
     <main className="w-full h-screen bg-cyan-500">
-      <TextInput label="Bill" iconPath={dollarIcon} isInvalid={false} />
-      <br />
-      <TextInput label="Number of People" iconPath={personIcon} isInvalid={false} />
+      <TipField tip={tip} setTip={setTip} />
     </main>
   );
 }
